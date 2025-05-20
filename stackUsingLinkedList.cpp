b7cfa27ccafbedb96e8561502d51e8b4dba12c9e
Riky900 <rickytripamungkas666@gmail.com>
2025-05-20 12:22:38 +0700

    menghapus node dari memori saat pop untuk menghibdari memory leak

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -29,18 +29,17 @@ public:
         top = NULL;
     }
 
-   
-
-    void pop()
+    int push(int value)
     {
-        if (isEmpty())
-        {
-            cout << "Stack is Empty" << endl; 
-        }
-
-        cout << "Popped Value: " << top->data << endl;
-        top = top->next;
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
     }
+
+    
 
     void peek()
     {
